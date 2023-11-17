9f308800ae7284aab7bcc096de45d9966cc0741a
ElianGARRIGUES <elian.garrigues@hotmail.fr>
2023-11-17 01:38:14 +0100

    début front page authentification

---
+++ zth-front/src/views/connexion.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+
+function envoi() {
+  console.log(username, password)
+  // passer en php ?
+  username.value = ''
+  password.value = ''
+}
+</script>
+
+<template>
+
+<div class="connexion">
+  <h1>Connexion</h1>
+  <input v-model="username" placeholder="nom utilisateur">
+  <input v-model="password" type="password" placeholder="mot de passe">
+  <button @click="envoi()">Envoyer</button>
+
+  <p>Pas de compte ?</p>
+  
+  <nav>
+      <RouterLink to="/inscription">Inscrivez-vous !</RouterLink>
+  </nav>
+</div>
+
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ zth-front/src/views/inscription.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { ref } from 'vue'
+
+const mail = ref('')
+const username = ref('')
+const password = ref('')
+const error = ref('')
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+function envoi() {
+  if (!validateEmail(mail.value)){
+    error.value = "Entrez un email valide"
+  }
+  else if (username.value.length < 5){
+    error.value = "Entrez un nom d'utilisateur de plus de 5 caractères"
+    console.log(error.value)
+  }
+  else if (password.value.length < 5){
+    error.value = "Entrez un mot de passe de plus de 5 caractères"
+  }
+  else{
+    // envoi data php
+    mail.value = ''
+    username.value = ''
+    password.value = ''
+    error.value = ''
+  }
+
+}
+</script>
+
+<template>
+
+<div class="connexion">
+  <h1>Inscription</h1>
+  <input v-model="mail" placeholder="adresse mail">
+  <input v-model="username" placeholder="nom utilisateur">
+  <input v-model="password" type="password" placeholder="mot de passe">
+  <button @click="envoi()">Envoyer</button>
+  {{ error }}
+    <p>déjà un compte ?</p>
+    <nav>
+        <RouterLink to="/connexion">Connectez-vous !</RouterLink>
+    </nav>
+    </div>
+  
+  </template>
+
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
