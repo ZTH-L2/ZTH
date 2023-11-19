e0eb051fad1e81caff62fff88a8244dfb0fb9333
Aekhy <102717242+Aekhy@users.noreply.github.com>
2023-11-19 14:13:31 +0100

    testing

--- zth-front/src/main.js
+++ zth-front/src/main.js
@@ -1,14 +1,17 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// import Markdown from "vue3-markdown-it";
+import "highlight.js/styles/monokai.css";
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+// app.use(Markdown);
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
---
+++ zth-front/src/views/MarkdowntestView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Markdown :source="source" :html="true" />
+  </div>
+  <textarea v-model="source"></textarea>
+</template>
+
+<script setup>
+import Markdown from "vue3-markdown-it";
+import { ref } from "vue";
+
+const source = ref("y");
+const props = defineProps(["source"]);
+</script>
