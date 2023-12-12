4dd93c1454529c7a855b464364bbb1971a688b95
Aekhy <102717242+Aekhy@users.noreply.github.com>
2023-12-12 21:50:10 +0100

    Revert "Merge pull request #7 from ZTH-L2/195612122023"
    This reverts commit abf8b2c85ef61313b278ab251caa46cea985ae84, reversing
    changes made to 641eb49606a5bf4ddba7fae48b69d42db551a375.

--- zth-front/src/views/lire_post.vue
+++ zth-front/src/views/lire_post.vue
@@ -52,12 +52,9 @@ export default {
         linkify: true,
         typographer: true,
       });
-      const styleDefault = "<style>img{ width:100%; height:auto; }object{ width:100%; height:auto; }</style>";
+
       const htmlContent = md.render(stringHTML);
-      const sanitizedHtml = DOMPurify.sanitize(htmlContent, {ADD_TAGS: ["object","img"], ADD_ATTR:['data','src'],FORBID_TAGS: ['style']});
-
-      sanitizedHtml = styleDefault + sanitizedHtml;
-
+      const sanitizedHtml = DOMPurify.sanitize(htmlContent, { ADD_TAGS: ['object'], ADD_ATTR: ['data'] });
       return sanitizedHtml;
     },
     async noter(rating){
