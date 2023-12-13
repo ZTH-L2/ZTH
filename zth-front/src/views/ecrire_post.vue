3890f437118e5132873ff3c6531152f5127476c4
FM04 <fm40.ceron@gmail.com>
2023-12-13 21:11:03 +0100

    Update ecrire_post.vue

--- zth-front/src/views/ecrire_post.vue
+++ zth-front/src/views/ecrire_post.vue
@@ -15,6 +15,12 @@
       </div>
 
       <h2>Fichier disponible:</h2>
+      <div  v-if="this.PrintErrorTOGGLE" 
+            id="DivError"
+            @click="this.PrintErrorTOGGLE = false;"
+            >
+            <div v-html="this.PrintErrorString"></div>
+      </div>
       <div v-for="(item, index) in listeFichier" :key="index">
         <div class="file-link">
           <a :href=" urlStore.api + `/data/${this.postData['id_post']}/${item}`">{{ item }}</a>
@@ -22,6 +28,8 @@
           <p @click="LienMarkDownFichier(item)">Insérer dans le MarkDown</p>
         </div>
       </div>
+
+      
 
       <div class="button">
         <button type="button" @click="Envoie()">Save</button>
@@ -31,12 +39,7 @@
 
       </div>
 
-      <div  v-if="this.PrintErrorTOGGLE" 
-            id="DivError"
-            @click="this.PrintErrorTOGGLE = false;"
-            >
-            <div v-html="this.PrintErrorString"></div>
-      </div>
+      
     </div>
   </div>
 </template>
@@ -364,12 +367,13 @@ p:hover {
   background-color: red;
 
   position:fixed;
-
+  /*
   height:90vh;
   width :90vw;
   bottom: calc(50% - (90vh/2));
   right: calc(50% - (90vw/2)) ;
-		
+	*/
+  width: 100%;
   overflow: auto;
 }
 
